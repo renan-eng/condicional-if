566ec765d60406c436a72166c8ef5c26b43a385e
renan-eng <renan3838@gmail.com>
2023-01-20 13:19:20 -0300

    condidional if else em rust básico

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let n = 35;
+
+    if n < 30 {
+        println!("O número é menor que 30!");
+    } else if n > 39 {
+        println!("O número é maior que 39!");
+    } else {
+        println!("O número não é menor que 30 ou maior que 39!");
+    }
+
+    if n == 20 {
+        println!("O número é igual a 20!");
+    }
+}
